42c1f3e35a614fbb92e4765fe95aa77f9583621c
sterfexe <thesterfrysoup@gmail.com>
2024-10-07 06:59:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        alert('Form submitted!');
+    });
+});
